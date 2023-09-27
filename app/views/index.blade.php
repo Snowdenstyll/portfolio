b37dc5510ae26685602bf43f61cb0f6c5e9dc465
Branden Vongphakdy <vbranden_@hotmail.com>
2023-09-26 22:31:57 -0400

    favicon

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -4,12 +4,12 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>{{ getenv('APP_NAME') ?? 'Leaf MVC' }}</title>
-		<link rel="shortcut icon" href="https://leafphp.dev/logo-circle.png" type="image/x-icon">
+		<title>{{ getenv('APP_NAME') }}</title>
+		<link rel="shortcut icon" href={{ assets("favicon/favicon-32x32.png")}} type="image/x-icon">
 
-		<link rel="apple-touch-icon" sizes="180x180" href="{{ assets("favicon/apple-touch-icon.png")}}">
-		<link rel="icon" type="image/png" sizes="32x32" href={{ assets("favicon-32x32.png")}}>
-		<link rel="icon" type="image/png" sizes="16x16" href={{ assets("favicon-16x16.png")}}>
+		<link rel="apple-touch-icon" sizes="180x180" href={{ assets("favicon/apple-touch-icon.png")}}>
+		<link rel="icon" type="image/png" sizes="32x32" href={{ assets("favicon/favicon-32x32.png")}}  type="image/x-icon">
+		<link rel="icon" type="image/png" sizes="16x16" href={{ assets("favicon/favicon-16x16.png")}}>
 		<link rel="manifest" href={{ assets("favicon/site.webmanifest")}}>
 
 		<link rel="stylesheet" href="{{ ViewsPath('css/template.css') }}">
