f0276d418feccccd07821925f5353bfc6101dca9
Branden Vongphakdy <vbranden_@hotmail.com>
2024-12-05 16:36:05 -0500

    wip

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
         args: 'start',
         env: {
             NODE_ENV: 'production',
-            NODE_OPTIONS: '--max-old-space-size=512'  // Reduced from 4096
+            NODE_OPTIONS: '--max-old-space-size=4096'  // Reduced from 4096
         },
         max_memory_restart: '800M'  // Reduced from 1G
     }]
